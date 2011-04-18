e1857c31ad61e04c8b0681d6297402f9c856ae0c
harism <harism@gmail.com>
2011-04-18 03:03:08 +0300

    Bitmap rendering 'improved'.

--- src/fi/harism/curl/CurlActivity.java
+++ src/fi/harism/curl/CurlActivity.java
@@ -3,6 +3,9 @@ package fi.harism.curl;
 import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
@@ -50,13 +53,37 @@ public class CurlActivity extends Activity {
 
 		@Override
 		public Bitmap getBitmap(int width, int height, int index) {
-			// Create Bitmap for renderer.
-			Drawable d = getResources().getDrawable(mBitmapIds[index]);
-			d.setBounds(0, 0, width, height);
 			Bitmap b = Bitmap.createBitmap(width, height,
 					Bitmap.Config.ARGB_8888);
-			b.eraseColor(0x00000000);
+			b.eraseColor(0xFFE0E0E0);
 			Canvas c = new Canvas(b);
+			Drawable d = getResources().getDrawable(mBitmapIds[index]);
+			
+			int margin = 7;
+			int border = 3;
+			Rect r = new Rect(margin, margin, width-margin, height-margin);
+			
+			int imageWidth = r.width() - (border * 2);
+			int imageHeight = imageWidth * d.getIntrinsicHeight() / d.getIntrinsicWidth();
+			if (imageHeight > r.height() - (border * 2)) {
+				imageHeight = r.height() - (border * 2);
+				imageWidth = imageHeight * d.getIntrinsicWidth() / d.getIntrinsicHeight();
+			}
+			
+			r.left += ((r.width() - imageWidth) / 2) - border;
+			r.right = r.left + imageWidth + border + border;
+			r.top += ((r.height() - imageHeight) / 2) - border;
+			r.bottom = r.top + imageHeight + border + border;
+			
+			Paint p = new Paint();
+			p.setColor(0xFFC0C0C0);
+			c.drawRect(r, p);
+			r.left += border;
+			r.right -= border;
+			r.top += border;
+			r.bottom -= border;
+			
+			d.setBounds(r);
 			d.draw(c);
 			return b;
 		}
